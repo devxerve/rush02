930ab33cb339f37a8db4103f20e66daf5369092c
Javier Cervera Rodriguez <jcervera@c2r10s4.42madrid.com>
2026-07-26 20:47:55 +0200

    backup

--- rush02/aux.c
+++ rush02/aux.c
@@ -10,12 +10,32 @@ int	ft_strcmp(char *s1, char *s2)
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
-int     ft_len(char *str)
+int	ft_len(char *str)
 {
-        int     i;
+	int	i;
 
-        i = 0;
-        while (str[i] != '\0')
-                i++;
-        return (i);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	length;
+	int	i;
+
+	length = 0;
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i + length] = src[i];
+		i++;
+	}
+	dest[i + length] = '\0';
+	return (dest);
 }
--- rush02/numbers.h
+++ rush02/numbers.h
@@ -11,17 +11,11 @@ typedef struct s_dict
 	char	*value;
 }			t_dict;
 
-int	ft_strcmp(char *s1, char *s2)
-{
-	int	i;
+int			ft_strcmp(char *s1, char *s2);
 
-	i = 0;
-	while ((s1[i] != '\0') && (s1[i] == s2[i]))
-	{
-		i++;
-	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-}
+int			ft_len(char *str);
+
+char		*ft_strcat(char *dest, char *src);
 
 void		*ft_solve_number(char *num_str, t_dict *dict, int total_elements);
 
